70482a4f9621ad2cc2b97d7bd5411573ec8ec39f
Michael Galero <4162+mikong@users.noreply.github.com>
2024-03-16 20:05:07 +0800

    feat: Add create tenant in Tenants API

--- tenants.py
+++ tenants.py
@@ -1,23 +1,33 @@
 from uuid import uuid4
+from flask import abort
 
-TENANTS = {
-    "Cisco": {
-        "id": str(uuid4()),
-        "name": "Cisco Systems, Inc."
-    },
-    "Cigna": {
-        "id": str(uuid4()),
-        "name": "The Cigna Group"
-    },
-    "ATT": {
-        "id": str(uuid4()),
-        "name": "AT&T Inc."
-    },
-    "Microsoft": {
-        "id": str(uuid4()),
-        "name": "Microsoft Corporation"
+TENANT_NAMES = [
+    "Cisco Systems, Inc.",
+    "The Cigna Group",
+    "AT&T Inc.",
+    "Microsoft Corporation"
+]
+
+TENANTS = {}
+for name in TENANT_NAMES:
+    id = str(uuid4())
+    TENANTS[id] = {
+        "id": id,
+        "name": name
     }
-}
 
 def index():
     return list(TENANTS.values())
+
+def create(tenant):
+    name = tenant.get("name")
+
+    if name is None or name == "":
+        abort(406, "Name is missing")
+
+    id = str(uuid4())
+    TENANTS[id] = {
+        "id": id,
+        "name": name
+    }
+    return TENANTS[id], 201
